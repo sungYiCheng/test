98070d10c1a14ac1b60f0d4f13fcefa961925db5
bbking <squalllucky@gmail.com>
2023-08-18 14:09:09 +0800

    change terraform and add Jenkinfile

--- Terraform/terrafform.tf
+++ Terraform/terrafform.tf
@@ -1,27 +1,28 @@
 provider "google" {
-  credentials = file("account.json")
-  project = "conductive-coil-323706"
+  credentials = "${file("account.json")}"
+  project = "wt-gd-pdd2"
   region  = "asia-east1"
-  zone    = "asia-east1-c"
+  zone    = "asia-east1-a"
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "bbtest09"
-  machine_type = "e2-standard-4"
+resource "google_compute_instance" "test_vm" {
+  name         = "hrs-dev-deploy-linux-asia-east1-a-1-test3"
+  machine_type = "e2-standard-2"
+  public_ips     = ["34.81.76.129"]
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-10"
+       size = 50
+       image = "debian-cloud/debian-11"
     }
   }
 
-  tags = ["nginxtest", "http-server", "https-server"]
+  tags = ["cm-all-allow", "http-server", "https-server"]
 
   network_interface {
     # A default network is created for all GCP projects
     network       = "default"
     access_config {
-      // Ephemeral IP
     }
   }
 }
